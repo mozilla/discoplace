90363f73853b839b1d49242cddd1006c9ff1d1d2
Mathieu Pillard <mpillard@mozilla.com>
2014-08-29 19:21:38 +0200

    Use new feed collection API (bug 1048366)

--- src/media/js/routes_api.js
+++ src/media/js/routes_api.js
@@ -1,6 +1,6 @@
 define('routes_api', [], function() {
     return {
-        'collection': '/api/v1/fireplace/collection/{0}/?cache=1&vary=0',
+        'collection': '/api/v2/fireplace/feed/collections/{0}/?cache=1&vary=0',
         'newsletter': '/api/v1/account/newsletter/'
     };
 });
